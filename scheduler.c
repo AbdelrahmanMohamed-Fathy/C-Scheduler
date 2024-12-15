4d34d34cf00bb382f1eab9e2f0cbf5f74ec92e71
AbdelrahmanMohamed-Fathy <abdelrahman.fathy04@outlook.com>
2024-12-15 18:29:04 +0200

    scheduler initializer

--- scheduler.c
+++ scheduler.c
@@ -1,20 +1,36 @@
-#include "headers.h"
 #include "DataStructures/priQueue.h"
+#include "headers.h"
+#include "cpuData.h"
 #include "pcb.h"
+#include "hpf.h"
 
 void clearResources(int signum);
 
 int ProcessMessageQueue;
 
+// TODO: implement the scheduler.
+// TODO: upon termination release the clock resources.
 int main(int argc, char *argv[])
 {
     signal(SIGINT, clearResources);
     initClk();
-
+    Algorithm SchedulingAlgorithm = atoi(argv[1]);
+    int Quantum = atoi(argv[2]);
     ProcessMessageQueue = msgget(MSGKEY, IPC_CREAT | 0666);
-    SJF();
-    // TODO: implement the scheduler.
-    // TODO: upon termination release the clock resources.
+
+    switch (SchedulingAlgorithm)
+    {
+    case Shortest_Job_First:
+        SJF();
+        break;
+    case Premptive_Highest_Priority_First:
+        HPF(ProcessMessageQueue);
+        break;
+    case Round_Robin:
+        break;
+    case Multiple_Level_Feedback_Loop:
+        break;
+    }
 
     printf("scheduler terminating normally.\n");
     destroyClk(false);
@@ -25,39 +41,4 @@ void clearResources(int signum)
 {
     msgctl(ProcessMessageQueue, IPC_RMID, NULL);
     exit(1);
-}
-
-void SJF()
-{
-    msg sjfmsg;
-    priQueue *sjfqueue;
-    PCB *runningprocess;
-    sjfqueue = CreatePriQueue();
-    while (msgrcv(ProcessMessageQueue,&sjfmsg,sizeof(struct msg),0,IPC_NOWAIT)!=-1)
-    {
-        runningprocess = (PCB*)malloc(sizeof(PCB));
-        runningprocess->ID = sjfmsg.data.id;
-        runningprocess->Priority = sjfmsg.data.runningtime;
-        runningprocess->ArrivalTime = sjfmsg.data.arrivaltime;
-        runningprocess->RunningTime=sjfmsg.data.runningtime;
-        runningprocess->RemainingTime=sjfmsg.data.runningtime;
-        runningprocess->StartTime= -1;
-        runningprocess->EndTime = -1;
-        runningprocess->WaitTime = 0;
-        PriEnqueue(sjfqueue,&runningprocess,runningprocess->Priority);
-        //schmsg.data.processinfo.RemainingTime = schmsg.data.runningtime;
-    }
-
-    while (PriDequeue(sjfqueue,&runningprocces))
-    {
-        while (runningprocces->processinfo.RemainingTime > 0)
-        {
-            int time;
-            printf("process numer with id %d and runtime %d is running\n ",runningprocces->id,runningprocces->runningtime);
-            time = getClk();
-            printf("remaining time = %d      clock =%d ",runningprocces->processinfo.RemainingTime,time);
-            runningprocces->processinfo.RemainingTime-- ;
-        }
-        printf("sjf done");
-    }
 }
